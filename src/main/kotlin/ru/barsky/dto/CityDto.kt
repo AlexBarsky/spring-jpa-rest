f8a2da5507f221921f9a7c9b544153f394bfd226
AlexBarsky <bogomolov2004alex@gmail.com>
2024-02-25 04:43:36 -0800

    Project: Dto validation

--- src/main/kotlin/ru/barsky/dto/CityDto.kt
+++ src/main/kotlin/ru/barsky/dto/CityDto.kt
@@ -1,5 +1,10 @@
 package ru.barsky.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+
 data class CityDto(
+    @NotBlank
+    @Min(2)
     val name: String,
 )
--- src/main/kotlin/ru/barsky/dto/CountryDto.kt
+++ src/main/kotlin/ru/barsky/dto/CountryDto.kt
@@ -1,8 +1,21 @@
 package ru.barsky.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+
 data class CountryDto(
     val id: Int? = null,
+
+    @NotBlank
+    @Min(2)
     val name: String,
+
+    @NotNull
+    @Min(1)
     val population: Int,
+
+    @NotEmpty
     val cities: List<CityDto>,
 )
